ef1c13be2303ffa45b03fdfdc4e56f45ec360a89
hmia19951-create <hmia19951@gmail.com>
2026-08-08 16:36:31 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -196,3 +196,12 @@ dependencies {
     androidTestImplementation(libs.hilt.android.testing)
     kspAndroidTest(libs.hilt.compiler)
 }
+dependencies {
+    // Retrofit & Gson for API connection
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    
+    // OkHttp & Logging Interceptor
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+}
